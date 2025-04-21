8408974781f24df482c5ce392861b836165b9852
Milton Araújo <miltonjr.dev@gmail.com>
2025-04-21 17:24:22 -0300

    new form test

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('src/index.html')
+  })
+
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Milton')
+    cy.get('#lastName').type('Araújo')
+    cy.get('#email').type('miltonjr.dev@gmail.com')
+    cy.get('#open-text-area').type('Obrigado!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
