235fb67c3d1ccc4aeb6b7dfec4638ac0977576c5
Milton Araújo <miltonjr.dev@gmail.com>
2025-04-22 15:52:15 -0300

    extra exercise 1, add delay to text-area field

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -8,10 +8,12 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvxwyz', 10)
+
     cy.get('#firstName').type('Milton')
     cy.get('#lastName').type('Araújo')
     cy.get('#email').type('miltonjr.dev@gmail.com')
-    cy.get('#open-text-area').type('Obrigado!')
+    cy.get('#open-text-area').type(longText, {delay: 0})
     cy.get('button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
